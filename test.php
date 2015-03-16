df2de3234b885dd3aaa054b349abfdb70d5354a5
Dana Comstock <justsparx@gmail.com>
2015-03-16 16:32:18 -0700

    basic html structure commit

--- test.php
+++ test.php
@@ -1,3 +1,13 @@
 <?php
-  phpinfo();
+  session_start();
 ?>
+<!doctype html>
+<html>
+	<head>
+		<title>Website</title>
+	</head>
+	<body>
+		<h3>This is a website</h3>
+		<p>This is some supporting text for the website.</p>
+	</body>
+</html>
